5427f0bb117d0a12a30a539322ba37b7d6a0ef70
cysabi <47790183+cysabi@users.noreply.github.com>
2024-11-11 16:05:38 -0500

    start new

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,6 @@
-import { MarkdownPostProcessor, Plugin } from "obsidian";
-import { DataArray, getAPI, Link, Literal } from "obsidian-dataview";
+import { Plugin, ItemView, WorkspaceLeaf, IconName } from "obsidian";
+import { DataArray, Link } from "obsidian-dataview";
 import "./styles.css";
-import {
-  ViewUpdate,
-  PluginValue,
-  EditorView,
-  ViewPlugin,
-} from "@codemirror/view";
-import { syntaxTree } from "@codemirror/language";
-import {
-  Extension,
-  RangeSetBuilder,
-  StateField,
-  Transaction,
-} from "@codemirror/state";
-import { Decoration, DecorationSet, WidgetType } from "@codemirror/view";
-
 type Pebl = {
   file: {
     link: Link;
@@ -24,89 +9,104 @@ type Pebl = {
   };
 };
 
-export class PeblWidget extends WidgetType {
-  toDOM(view: EditorView): HTMLElement {
-    const div = document.createElement("input");
-    div.style.display = "inline-block";
-    div.type = "checkbox";
-    return div;
+export class PeblView extends ItemView {
+  constructor(leaf: WorkspaceLeaf) {
+    super(leaf);
   }
+
+  getIcon() {
+    return "shapes";
+  }
+
+  getViewType() {
+    return "pebl";
+  }
+
+  getDisplayText() {
+    return "Pebl";
+  }
+
+  async onOpen() {
+    const container = this.containerEl.children[1];
+    container.empty();
+    container.createEl("h4", { text: "Example view" });
+  }
+
+  async onClose() {}
 }
-
-export const peblStateView = StateField.define<DecorationSet>({
-  create(state): DecorationSet {
-    return Decoration.none;
-  },
-  update(oldState: DecorationSet, transaction: Transaction): DecorationSet {
-    const builder = new RangeSetBuilder<Decoration>();
-
-    syntaxTree(transaction.state).iterate({
-      enter(node) {
-        if (node.type.name.startsWith("hmd-internal-link")) {
-          builder.add(
-            node.from,
-            node.to,
-            Decoration.mark({
-              inclusive: true,
-              class: "pebl",
-            })
-          );
-        }
-      },
-    });
-    return builder.finish();
-  },
-  provide(field: StateField<DecorationSet>): Extension {
-    return EditorView.decorations.from(field);
-  },
-});
 
 export default class PeblPlugin extends Plugin {
   async onload() {
-    this.registerEditorExtension(peblStateView);
-    this.registerMarkdownPostProcessor((el, ctx) => {
-      const tree: (Pebl & { depth: number })[] = [];
+    console.log(
+      this.app.workspace.getLeavesOfType("file-explorer")?.first()?.view
+    );
+  }
 
-      const dv = getAPI(this.app);
-      const getSubs = (
-        pages: DataArray<Record<string, Literal>>,
-        depth: number = 0
-      ) => {
-        const pebls = pages
-          .where((page) => page["pebl.task"] != undefined)
-          .file.link.map((link: Link) => dv.page(link)) as DataArray<Pebl>;
+  async onunload() {}
 
-        pebls.forEach((pebl) => {
-          tree.push({ ...pebl, depth });
-          getSubs(
-            pebl.file.outlinks.map((out) => dv.page(out)),
-            depth + 1
-          );
-        });
-      };
-      getSubs(dv.pages("outgoing([[]])", ctx.sourcePath));
+  async activateView() {
+    const { workspace } = this.app;
 
-      console.log(tree);
-      console.log(el.findAll(".internal-link"));
+    let leaf: WorkspaceLeaf | null = null;
+    const leaves = workspace.getLeavesOfType("pebl");
 
-      el.findAll(".internal-link").map((link) => {
-        const data = tree.find((t) => t.file.name === link.dataset.href);
-        if (data) {
-          const wrap = document.createElement("span");
-          wrap.className = "pebl";
-          const checkbox = document.createElement("input");
-          checkbox.type = "checkbox";
-          checkbox.checked = data.pebltask;
-          checkbox.onchange = (ev) => {
-            ev.preventDefault();
-          };
+    if (leaves.length > 0) {
+      // A leaf with our view already exists, use that
+      leaf = leaves[0];
+    } else {
+      // Our view could not be found in the workspace, create a new leaf
+      // in the right sidebar for it
+      leaf = workspace.getRightLeaf(false);
+      await leaf.setViewState({ type: "pebl", active: true });
+    }
 
-          link.parentNode.insertBefore(wrap, link);
-
-          wrap.appendChild(checkbox);
-          wrap.appendChild(link);
-        }
-      });
-    });
+    // "Reveal" the leaf in case it is in a collapsed sidebar
+    workspace.revealLeaf(leaf);
   }
 }
+
+// this.registerEditorExtension(peblStateView);
+// this.registerMarkdownPostProcessor((el, ctx) => {
+//   const tree: (Pebl & { depth: number })[] = [];
+
+//   const dv = getAPI(this.app);
+//   const getSubs = (
+//     pages: DataArray<Record<string, Literal>>,
+//     depth: number = 0
+//   ) => {
+//     const pebls = pages
+//       .where((page) => page["pebl.task"] != undefined)
+//       .file.link.map((link: Link) => dv.page(link)) as DataArray<Pebl>;
+
+//     pebls.forEach((pebl) => {
+//       tree.push({ ...pebl, depth });
+//       getSubs(
+//         pebl.file.outlinks.map((out) => dv.page(out)),
+//         depth + 1
+//       );
+//     });
+//   };
+//   getSubs(dv.pages("outgoing([[]])", ctx.sourcePath));
+
+//   console.log(tree);
+//   console.log(el.findAll(".internal-link"));
+
+//   el.findAll(".internal-link").map((link) => {
+//     const data = tree.find((t) => t.file.name === link.dataset.href);
+//     if (data) {
+//       const wrap = document.createElement("span");
+//       wrap.className = "pebl";
+//       const checkbox = document.createElement("input");
+//       checkbox.type = "checkbox";
+//       checkbox.checked = data.pebltask;
+//       checkbox.onchange = (ev) => {
+//         ev.preventDefault();
+//       };
+
+//       link.parentNode.insertBefore(wrap, link);
+
+//       wrap.appendChild(checkbox);
+//       wrap.appendChild(link);
+//     }
+//   });
+// });
